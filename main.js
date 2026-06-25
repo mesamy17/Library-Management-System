387cc7765ebaf9380b4ac34aba3eec68957b7186
Samarth Sawant <sawant.samarths@gmail.com>
2026-06-26 02:35:28 +0530

    add class Member

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ class Book {
     this.isbn = isbn;
   }
 }
+
+class Member {
+  constructor(name) {
+    this.name = name;
+  }
+
+  borrowBook(book) {
+    return this.name + "borrowed" + this.name;
+  }
+
+  returnBook(book) {
+    return this.name + "returned" + this.book;
+  }
+}
